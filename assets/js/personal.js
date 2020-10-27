7bf8bf56890fdc39c5819157c6f789e446be92d0
keely_lee <keely_lee@outlook.com>
2020-10-27 14:02:12 -0400

    Test addtl docs

---
+++ assets/js/personal.js
@@ -0,0 +1,15 @@
+      // <script>
+      //   function test() {
+      //     const grab = document.getElementById("contact-wrapper");
+      //     grab.classList.toggle("flip-down");
+      //     grab.classList.toggle("flip-up");
+      //   }
+      // </script>
+
+
+
+(function test() {
+  $("#contact-wrapper").toggleClass("flip-down");
+  $("#contact-wrapper").toggleClass("flip-up");
+
+})(jQuery);
