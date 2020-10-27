92d7326981fa5b8b4a6ed01e06a41046506b7bc9
keely_lee <keely_lee@outlook.com>
2020-10-27 19:31:35 -0400

    Add link animation

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -12,17 +12,23 @@
   $(".project-desc").hide();
 
   $(".project-div").hover( 
-
-    function() {
+    function() { //mousein
       const divHeight = $(this).height();
       $(this).height(divHeight);
       $(this).toggleClass("project-hover");
       $("img", this).toggleClass("shrink");
       $(".project-desc", this).show();
-    }, function() { 
+
+    }, function() {  //mouseout
       $(".project-desc").hide(); 
       $(this).toggleClass("project-hover");
       $("img", this).toggleClass("shrink");
     }
   )
+
+    $(".project-desc > a").hover(
+      function(){ $(this).toggleClass("link-flip"); }, 
+      function(){ $(this).toggleClass("link-flip"); }
+    )
+
 })(jQuery);
